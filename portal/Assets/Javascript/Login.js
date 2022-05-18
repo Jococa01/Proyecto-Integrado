a2078e2e2c6be7a7a12fe206d140ade2cdb28bbb
Jococa01 <jococa01@floridauniversitaria.es>
2022-05-18 11:55:43 +0200

    Redirección

--- portal/Assets/Javascript/Login.js
+++ portal/Assets/Javascript/Login.js
@@ -51,7 +51,7 @@ function CheckPassword(data){
     if(data['contrasenya']==md5(password.value)){
         console.log("Pass correcta, se introdujo: "+md5(password.value)+" y la contraseña era: "+data['contrasenya']);
         StartSession(data);
-        // window.location.href="./index.html";
+        window.location.href="./index.html";
     }else{
         console.log("Pass incorrecta, se introdujo: "+md5(password.value)+" y la contraseña era: "+data['contrasenya']);
     }
